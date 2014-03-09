aeec6e582b51d2bb99d0567cc4c3be237d705d25
shiyinayuriko <whitecometliliwill@gmail.com>
2014-03-09 21:13:22 +0800

    sever code+server doc
    balabalabalabalabbabababiu~

---
+++ UPLOAD/模块开发者-张文戎/server code/fMail.php
@@ -0,0 +1,11 @@
+<?php
+// 调试模式, 上线时候关闭
+define('APP_DEBUG', true);
+// 应用名称
+// define('APP_NAME', 'Lemon');
+// 应用根路径
+// define('APP_ROOT_PATH', realpath('.') . '/');
+// 应用相对路径
+define('APP_PATH', './'.'fMail'.'/');
+header("Access-Control-Allow-Origin: *");
+require '/ThinkPHP/ThinkPHP.php';
